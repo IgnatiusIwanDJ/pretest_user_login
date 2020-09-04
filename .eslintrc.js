818d4829dd5bfc1a1236c52c0f836fe476cdacea
Ignatius Iwan DJ <ignatius.iwan@mail.ugm.ac.id>
2020-09-04 21:41:23 +0700

    feature-edit-esLintRules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,10 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
 }
